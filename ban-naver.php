136c75794d6b5ace5c878d48f3ee3b84c9bd3f9a
hideokamoto <hide.okamoto@digitalcube.jp>
2016-12-07 17:30:39 +0900

    add filter

--- ban-naver.php
+++ ban-naver.php
@@ -15,7 +15,13 @@
 add_filter( 'the_content', 'ban_naver');
 function ban_naver( $content ) {
 	$ban_word = __( '※このページ内の画像をNAVERまとめに転載することを禁止します。', 'ban-naver');
+	$ban_word = apply_filters( 'ban_word_test', $ban_word );
 	$ban_word = "<small><b>{$ban_word}</b></small>";
-	$content =  $ban_word. $content;
+	$ban_position = apply_filters( 'ban_word_position', 'top' );
+	if ( 'bottom' === $ban_position ) {
+		$content = $content. $ban_word;
+	} else {
+		$content = $ban_word. $content;
+	}
 	return $content;
 }
