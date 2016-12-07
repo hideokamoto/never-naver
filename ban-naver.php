15b31561cce3edfd20c4ec41b677fa3df2239f9d
hideokamoto <hide.okamoto@digitalcube.jp>
2016-12-07 17:48:55 +0900

    add esc

--- ban-naver.php
+++ ban-naver.php
@@ -15,7 +15,7 @@
 add_filter( 'the_content', 'ban_naver');
 function ban_naver( $content ) {
 	$ban_word = __( '※このページ内の画像をNAVERまとめに転載することを禁止します。', 'ban-naver');
-	$ban_word = apply_filters( 'ban_word_text', $ban_word );
+	$ban_word = esc_html( apply_filters( 'ban_word_text', $ban_word ) );
 	$ban_word = "<small><b>{$ban_word}</b></small>";
 	$ban_position = apply_filters( 'ban_word_position', 'top' );
 	if ( 'bottom' === $ban_position ) {
